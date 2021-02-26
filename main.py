f92ff93397b9668c92adce7246c647acb1a0982f
WillCPP <WillCPP@users.noreply.github.com>
2021-02-26 00:27:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from random import randint
 from random import choice
-import sys
+# from queue import LifoQueue
+# import sys
+import threading
+import os
+import logging
 
 # sys.setrecursionlimit(4000)
 
@@ -9,8 +13,10 @@ PRINT_FLAG = False
 class Experiment:
     def __init__(self):
         self.nodes_sent = []
+        self.nodes_sent_2 = {}
 
     def process1(self, t):
+        logging.info('here_p')
         return randint(0, len(t)-1)
 
     def process2(self, t):
@@ -34,6 +40,7 @@ class Experiment:
         if i == 0:
             self.climbDown(t, 2*i+1)
             self.climbDown(t, 2*i+2)
+            # self.climbDown2(t, i)
         else:
             self.perfromMarkingRules(t, i)
             if PRINT_FLAG: print(f'U: {t}')
@@ -46,8 +53,19 @@ class Experiment:
             self.climbDown(t, 2*i+1)
             self.climbDown(t, 2*i+2)
 
+    # def climbDown2(self, t, i):
+    #     stack = LifoQueue(maxsize=len(t))
+    #     stack.put(i)
+    #     while not stack.empty():
+    #         current = stack.get()
+    #         self.perfromMarkingRules(t, current)
+    #         if 2*current+2 < len(t):
+    #             stack.put(2*current+1)
+    #             stack.put(2*current+2)
+
     def perfromMarkingRules(self, t, i):
         # Check if current node is marked
+        if i == 0: return
         if t[i] == 1:
             # Check if current node is a left or right sibling
             if i % 2 == 0:
@@ -78,38 +96,87 @@ class Experiment:
     def mark(self, t, i):
         if t[i] == 1: return
         t[i] = 1
+        logging.info('here_m')
         if PRINT_FLAG: print('==================================================')
         if PRINT_FLAG: print(f'M: {t}')
         self.perfromMarkingRules(t, i)
         if PRINT_FLAG: print(f'N: {t}')
         self.climbUp(t, i if i==0 else (i-1)//2)
 
-    def run(self, process):
-        for trial in range(0, 10):
-            print(f'    TRIAL {trial}')
-            for n in range(10, 21):
-            # for n in range(10, 11):#21):
-                print(f'        FOR n = {n}')
-                if process == e.process2: self.nodes_sent = []
-                N = 2**n - 1
-                binary_tree = [0]*(N)
-                steps = 0
-                while 0 in binary_tree:
-                    steps += 1
-                    i = process(binary_tree)
-                    self.mark(binary_tree, i)
-                print(f'            STEPS: {steps}')
+    # def run(self, process, p):
+    #     for trial in range(0, 10):
+    #         print(f'    TRIAL {trial}')
+    #         for n in range(10, 21):
+    #         # for n in range(10, 13):#21):
+    #             # print(f'        FOR n = {n}')
+    #             # if process == e.process2: self.nodes_sent = []
+    #             # N = 2**n - 1
+    #             # binary_tree = [0]*(N)
+    #             # steps = 0
+    #             # while 0 in binary_tree:
+    #             #     steps += 1
+    #             #     i = process(binary_tree)
+    #             #     self.mark(binary_tree, i)
+    #             # print(f'            STEPS: {steps}')
+    #             t = threading.Thread(target=self.runInThread, name=f'=process_{p}_trial_{trial}_n_{n}', args=(self, p,trial,n,))
+    #             t.daemon = True
+    #             t.start()
+    
+    # def runInThread(self, process, p, trial, n):
+    #     # print(f'        FOR n = {n}')
+    #     if process == e.process2: self.nodes_sent = []
+    #     N = 2**n - 1
+    #     binary_tree = [0]*(N)
+    #     steps = 0
+    #     while 0 in binary_tree:
+    #         steps += 1
+    #         i = process(binary_tree)
+    #         self.mark(binary_tree, i)
+    #     # print(f'            STEPS: {steps}')
+    #     s = f'{steps}'
+    #     with open(f'results_{p}_{trial}_{n}', 'w') as file_object:
+    #         file_object.write(s)
 
-e = Experiment()
+def runTrial(p, trial, n):
+    print(os.getcwd())
+    e = Experiment()
+    if p == '1': process = e.process1
+    if p == '2': process = e.process2
+    if p == '3': process = e.process3
+    N = 2**n - 1
+    binary_tree = [0]*(N)
+    steps = 0
+    print('here1')
+    while 0 in binary_tree:
+        steps += 1
+        i = process(binary_tree)
+        e.mark(binary_tree, i)
+    print('here2')
+    with open(f'results_{p}_{trial}_{n}.txt', 'w') as file_object:
+            file_object.write(f'{steps}')
+    print('DONE')
+
+def run_p(p):
+    for trial in range(0, 1):
+        for n in range(10, 12):
+            t = threading.Thread(target=runTrial, name=f'=process_{p}_trial_{trial}_n_{n}', args=(p, trial, n,))
+            # t.daemon = True
+            t.start()
+    
+        
+# e = Experiment()
 print('==================================================')
 print('PROCESS 1')
 print('==================================================')
-e.run(e.process1)
+# e.run(e.process1, '1')
+run_p('1')
 print('==================================================')
 print('PROCESS 2')
 print('==================================================')
-e.run(e.process2)
+# e.run(e.process2, '2')
+run_p('2')
 print('==================================================')
 print('PROCESS 3')
 print('==================================================')
-e.run(e.process3)
+# e.run(e.process3, '3')
+run_p('3')
